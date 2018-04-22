469a97f95927c70665655a373d8c2951d239b96a
YegorN <36129602+YegorN@users.noreply.github.com>
2018-04-22 19:49:36 +0300

    create Calculate

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,5 +1,19 @@
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Calculate...");
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int summ = first + second;
+        int difference = first - second;
+        int mltple = first * second;
+        int dvsn = first / second;
+        System.out.println("Summ = " + summ + " dffrnce = " + difference + " mltple = " + mltple + " dvsn = " + dvsn);
+
+        double firstDble = Double.valueOf(args[2]);
+        double secondDble = Double.valueOf(args[3]);
+        double summDble = firstDble + secondDble;
+        double dffrncDble = firstDble - secondDble;
+        System.out.println("SummDble = " + summDble + " dffrncDble = " + dffrncDble);
+
     }
 }
